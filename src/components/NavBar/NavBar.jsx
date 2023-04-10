9748d24fe38453b67096af028cc10661c7656fa2
Gonzor <gonzalo.marodriguez@gmail.com>
2023-04-10 10:54:53 -0300

    cartWidget created

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidget from '../CartWidget/CartWidget';
 
 const NavBar = () => {
     return (
@@ -14,23 +15,14 @@ const NavBar = () => {
               <Nav.Link href="#features">Historia</Nav.Link>
               <Nav.Link href="#pricing">Nosotros</Nav.Link>
               <NavDropdown title="Categorias" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Categorias</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Indumentaria</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
-                  Another action
+                  Accesorios
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Calzados</NavDropdown.Item>
               </NavDropdown>
             </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
+            <CartWidget />
           </Navbar.Collapse>
         </Container>
       </Navbar>
